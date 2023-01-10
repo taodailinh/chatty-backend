615d7905295819a7c213f00300d062b1078d0e30
Linh <taodailinhsuper@gmail.com>
2023-01-10 21:21:21 +0700

    set up database

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
 import express, { Express } from "express";
 import { ChattyServer } from "./setupServer";
-class Application {}
+class Application {
+  public initialize(): void {
+    const app: Express = express();
+    const server: ChattyServer = new ChattyServer(app);
+    server.start();
+  }
+}
+
+const application: Application = new Application();
+application.initialize();
