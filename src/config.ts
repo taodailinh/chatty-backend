051403d9e35af7577ea647fa16994def0fd4c824
Linh <taodailinhsuper@gmail.com>
2023-01-11 21:20:57 +0700

    Set up config

--- src/config.ts
+++ src/config.ts
@@ -5,6 +5,33 @@ dotenv.config({});
 
 class Config {
   public DATABASE_URL: string | undefined;
+  public JWT_TOKEN: string | undefined;
+  public NODE_ENV: string | undefined;
+  public SECRET_KEY_ONE: string | undefined;
+  public SECRET_KEY_TWO: string | undefined;
+  public CLIENT_URL: string | undefined;
+
+  private readonly DEFAULT_DATABASE_URL =
+    "mongodb://localhost:27017/chatty-backend";
+
+  constructor() {
+    this.DATABASE_URL = process.env.DATABASE_URL || this.DEFAULT_DATABASE_URL;
+    this.JWT_TOKEN = process.env.JWT_TOKEN || "";
+    this.NODE_ENV = process.env.NODE_ENV || "";
+    this.SECRET_KEY_ONE = process.env.SECRET_KEY_ONE || "";
+    this.SECRET_KEY_TWO = process.env.SECRET_KEY_TWO || "";
+    this.CLIENT_URL = process.env.CLIENT_URL || "";
+  }
+
+  public validateConfig(): void {
+    // Destructure this into array of keys (environment variables) and its value
+    for (const [key, value] of Object.entries(this)) {
+      if (value === undefined) {
+        // throw an error if one of them is undefined
+        throw new Error(`Configuration ${key} is undefined`);
+      }
+    }
+  }
 }
 
 export const config: Config = new Config();
