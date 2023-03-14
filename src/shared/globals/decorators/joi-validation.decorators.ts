3c443ffe17172a90e0529e3182e0cadf36793ecf
Linh <taodailinhsuper@gmail.com>
2023-03-14 22:02:56 +0700

    add auth interface and auth schema

--- src/shared/globals/decorators/joi-validation.decorators.ts
+++ src/shared/globals/decorators/joi-validation.decorators.ts
@@ -1,14 +1,23 @@
 import { JoiRequestValidationError } from '@global/helpers/error-handler';
 import { Request } from 'express';
-import { ObjectSchema } from 'joi';
+import Joi, { ObjectSchema } from 'joi';
 
 type IJoiDecorator = (target: any, key: string, descriptor: PropertyDescriptor) => void;
 
 export function joiValidation(schema: ObjectSchema): IJoiDecorator {
   return (_target: any, _key: string, descriptor: PropertyDescriptor) => {
     const originalMethod = descriptor.value;
+
+    // Pass all parameter from controller (req, res, next)
     descriptor.value = async function (...args: any[]) {
       const req: Request = args[0];
+      const { error } = await Promise.resolve(schema.validate(req.body));
+      if (error?.details) {
+        throw new JoiRequestValidationError(error.details[0].message);
+      }
+
+      return originalMethod.apply(this, args);
     };
+    return descriptor;
   };
 }
