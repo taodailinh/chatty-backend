1006273530a40151916ed420ae3fc6e1d5a5055e
Linh <taodailinhsuper@gmail.com>
2023-03-15 23:04:12 +0700

    add signup method

---
+++ src/features/auth/controllers/signup.ts
@@ -0,0 +1,51 @@
+import { ObjectId } from 'mongodb';
+import { Request, Response } from 'express';
+import { joiValidation } from '@global/decorators/joi-validation.decorators';
+import { signupSchema } from '@auth/schemes/signup';
+import { IAuthDocument, ISignUpData } from '@auth/interfaces/auth.interface';
+import { authService } from '@service/db/auth.service';
+import { BadRequestError } from '@global/helpers/error-handler';
+import { Helpers } from '@global/helpers/helper';
+import { UploadApiResponse } from 'cloudinary';
+import { uploads } from '@global/helpers/cloudinary-upload';
+
+export class SignUp {
+  @joiValidation(signupSchema)
+  public async create(req: Request, res: Response): Promise<void> {
+    const { username, password, email, avatarColor, avatarImage } = req.body;
+    const checkIfUserExist: IAuthDocument = await authService.getUserByUserNameOrEmail(username, email);
+    if (checkIfUserExist) {
+      throw new BadRequestError('User already exists');
+    }
+    const authObjectId: ObjectId = new ObjectId();
+    const userObjectId: ObjectId = new ObjectId();
+    const uId = `${Helpers.generateRandomIntegers(12)}`;
+
+    // Teacher use SignUp.prototype.signUpData({.....});
+    const authData: IAuthDocument = SignUp.prototype.signUpData({
+      _id: authObjectId,
+      uId,
+      username,
+      email,
+      password,
+      avatarColor
+    });
+
+    const result: UploadApiResponse = (await uploads(avatarImage, `${userObjectId}`, true, true)) as UploadApiResponse;
+    if (!result.public_id) {
+      throw new BadRequestError('Error upload the image. Try again');
+    }
+  }
+  private signUpData(data: ISignUpData): IAuthDocument {
+    const { _id, username, email, uId, password, avatarColor } = data;
+    return {
+      _id,
+      uId,
+      username: Helpers.firstLetterUppercase(username),
+      email: Helpers.lowerCase(email),
+      password,
+      avatarColor,
+      createdAt: new Date()
+    } as IAuthDocument;
+  }
+}
---
+++ src/shared/globals/helpers/helper.ts
@@ -0,0 +1,22 @@
+export class Helpers {
+  static firstLetterUppercase(str: string): string {
+    const valueString = str.toLocaleLowerCase();
+    return valueString
+      .split(' ')
+      .map((value: string) => `${value.charAt(0).toUpperCase()}${value.slice(1).toLowerCase()}`)
+      .join(' ');
+  }
+
+  static lowerCase(str: string): string {
+    return str.toLowerCase();
+  }
+  static generateRandomIntegers(integerLength: number): number {
+    const character = '0123456789';
+    let result = ' ';
+    const characterLenght = character.length;
+    for (let i = 0; i < characterLenght; i++) {
+      result += character.charAt(Math.floor(Math.random() * characterLenght));
+    }
+    return parseInt(result, 10);
+  }
+}
