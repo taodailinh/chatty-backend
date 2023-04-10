8a8a83f27ae373da99eb130183d5784a5bc89e28
Dai Linh <taodailinh@hotmail.com>
2023-04-10 22:30:45 +0700

    fix sign in bug - change return data from auth to user data

--- src/features/auth/controllers/signin.ts
+++ src/features/auth/controllers/signin.ts
@@ -4,9 +4,11 @@ import JWT from 'jsonwebtoken';
 import { ReasonPhrases, StatusCodes, getReasonPhrase, getStatusCode } from 'http-status-codes';
 import { config } from '@root/config';
 import { authService } from '@service/db/auth.service';
+import { userService } from '@service/db/user.service';
 import { BadRequestError } from '@global/helpers/error-handler';
 import { loginSchema } from '@auth/schemes/signin';
 import { IAuthDocument } from '@auth/interfaces/auth.interface';
+import { IUserDocument } from '@user/interfaces/user.interface';
 
 export class SignIn {
   @joiValidation(loginSchema)
@@ -20,9 +22,12 @@ export class SignIn {
     if (!passwordMatch) {
       throw new BadRequestError('Invalid credential');
     }
+
+    const user: IUserDocument = await userService.getUserByAuthId(`${existingUser._id}`);
+
     const userJwt: string = JWT.sign(
       {
-        userId: existingUser._id,
+        userId: user._id,
         uId: existingUser.uId,
         email: existingUser.email,
         username: existingUser.username,
@@ -31,6 +36,10 @@ export class SignIn {
       config.JWT_TOKEN!
     );
     req.session = { jwt: userJwt };
+    
+const userDocument: IUserDocument = {...user, username: existingUser!.username, authId: existingUser!._id, email: existingUser!.email, avatarColor: existingUser!.avatarColor, uId: existingUser!.uId, createdAt: existingUser!.createdAt} as IUserDocument;
+
+
     res.status(StatusCodes.OK).json({ message: 'Login successfully', user: existingUser, token: userJwt });
   }
 }
--- src/shared/services/db/user.service.ts
+++ src/shared/services/db/user.service.ts
@@ -1,9 +1,20 @@
+import { IAuthDocument } from '@auth/interfaces/auth.interface';
 import { IUserDocument } from '@user/interfaces/user.interface';
 import { UserModel } from '@user/models/user.schema';
+import mongoose from 'mongoose';
 
 class UserService {
   public async addUserData(data: IUserDocument): Promise<void> {
     await UserModel.create(data);
+  }
+  public async getUserById(userId: string): Promise<IUserDocument> {
+    const users: IUserDocument[] = await UserModel.aggregate([{$match:{_id: new mongoose.Types.ObjectId(userId)}}, {$lookup:{from: 'Auth', localField:'authId', foreignField: '_id', as: 'authId'}}, {$unwind: 'authId'}, {$project: this.aggregateProject()}]);
+    return users[0];
+  }
+
+  public async getUserByAuthId(authId: string): Promise<IUserDocument> {
+    const users: IUserDocument[] = await UserModel.aggregate([{$match:{authId: new mongoose.Types.ObjectId(authId)}}, {$lookup:{from: 'Auth', localField:'authId', foreignField: '_id', as: 'authId'}}, {$unwind: 'authId'}, {$project: this.aggregateProject()}]);
+    return users[0];
   }
 }
 
