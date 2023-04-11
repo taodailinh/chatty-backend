8aa0a027c15bd64119bd1673665074f331eb71c1
Dai Linh <taodailinh@hotmail.com>
2023-04-11 20:47:02 +0700

    add method to get cached user from redis

--- src/shared/services/redis/user.cache.ts
+++ src/shared/services/redis/user.cache.ts
@@ -3,6 +3,7 @@ import { IUserDocument } from '@user/interfaces/user.interface';
 import Logger from 'bunyan';
 import { config } from '@root/config';
 import { ServerError } from '@global/helpers/error-handler';
+import { Helpers } from '@global/helpers/helper';
 
 const log: Logger = config.createLogger('userCache');
 
@@ -92,5 +93,28 @@ export class UserCache extends BaseCache {
       log.error(error);
       throw new ServerError('Server error. Try again');
     }
+  }
+  public async getUserFromCache(key: string) : Promise<IUserDocument | null> {
+try {
+  if(!this.client.isOpen) {
+    await this.client.connect();
+  }
+  const response: IUserDocument = await this.client.HGETALL(`users:${key}`) as unknown as IUserDocument;
+  response.createdAt = new Date(Helpers.parseJson(`${response.createdAt}`));
+  response.postsCount = Helpers.parseJson(`${response.postsCount}`);
+  response.blocked = Helpers.parseJson(`${response.blocked}`);
+  response.blockedBy = Helpers.parseJson(`${response.blockedBy}`);
+  response.work = Helpers.parseJson(`${response.work}`);
+  response.school = Helpers.parseJson(`${response.school}`);
+  response.location = Helpers.parseJson(`${response.location}`);
+  response.quote = Helpers.parseJson(`${response.quote}`);
+  response.notifications = Helpers.parseJson(`${response.notifications}`);
+  response.social = Helpers.parseJson(`${response.social}`);
+  response.followersCount = Helpers.parseJson(`${response.followersCount}`);
+  response.followingCount = Helpers.parseJson(`${response.followingCount}`);
+} catch (error) {
+  log.error(error);
+  throw new ServerError('Server error. Try again!')
+}
   }
 }
