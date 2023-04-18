170b5a8a4cc0d1e1480986d9938f202dc5d0fe4b
Dai Linh <taodailinh@hotmail.com>
2023-04-18 21:48:34 +0700

    add some logger

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,13 @@ import express, { Express } from 'express';
 import { ChattyServer } from './setupServer';
 import databaseConnection from './setupDatabase';
 import { config } from './config';
+import Logger from 'bunyan';
+
+const log:Logger = config.createLogger('app.tss');
+
 class Application {
   public initialize(): void {
+    log.info('app initialize');
     this.loadConfig();
     databaseConnection();
     const app: Express = express();
